b8e263d9d9dc0bf2ddf9b9f10d0a00cbb11d9e3c
mengqiang <meng_qiang_vip@qq.com>
2025-05-12 11:12:15 +0800

    增加测试版本

--- hello/a.go
+++ hello/a.go
@@ -3,5 +3,9 @@ package hello
 import "fmt"
 
 func GetSay(name string) {
-	fmt.Printf("您的姓名是%s\n", name)
+	fmt.Printf("您的姓名是%s,很高兴认识你\n", name)
+}
+func Add(x int, y int) {
+	sum := x + y
+	fmt.Printf("和为%d\n", sum)
 }
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
-package gofirst
+package main
 
 import "github.com/meng-qiang-vip/gofirst/hello"
 
 func main() {
 
 	hello.GetSay("孟强")
+	hello.Add(10, 30)
 }
